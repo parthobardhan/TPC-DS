b07e82140352fce0e725037661ae784314d0a79c
Jemish Patel and Omer Arap <pair+jpatel-pivotal+oarap@pivotal.io>
2017-02-03 12:01:37 -0800

    Removed exit on PIPESTATUS

--- 05_sql/rollout.sh
+++ 05_sql/rollout.sh
@@ -30,7 +30,7 @@ for i in $(ls $PWD/*.$SQL_VERSION.*.sql); do
 
 	if [ "$EXPLAIN_ANALYZE" == "false" ]; then
 		echo "psql -A -q -t -P pager=off -v ON_ERROR_STOP=ON -v EXPLAIN_ANALYZE=\"\" -f $i | wc -l"
-		tuples=$(psql -A -q -t -P pager=off -v ON_ERROR_STOP=ON -v EXPLAIN_ANALYZE="" -f $i | wc -l; exit ${PIPESTATUS[0]})
+		tuples=$(psql -A -q -t -P pager=off -v ON_ERROR_STOP=ON -v EXPLAIN_ANALYZE="" -f $i | wc -l;)
 		#remove the extra line that \timing adds
 		tuples=$(($tuples-1))
 	else
