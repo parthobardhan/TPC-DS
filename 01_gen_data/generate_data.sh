580ff74a2f78d1aa9f0a2f828eea7b5766c5639d
Xin Zhang <xzhang@pivotal.io>
2018-07-27 16:34:34 -0500

    Increase dsdgen DOP 10x
    Co-authored-by: Xin Zhang <xzhang@pivotal.io>
    Co-authored-by: Goutam Tadi <gtadi@pivotal.io>
    
    Conflicts:
    	01_gen_data/generate_data.sh

--- 01_gen_data/generate_data.sh
+++ 01_gen_data/generate_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+GEN_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 GEN_DATA_SCALE=$1
 CHILD=$2
@@ -22,25 +22,30 @@ if [ "$PARALLEL" -eq "1" ]; then
 	SINGLE_SEGMENT="1"
 fi
 
-cd $PWD
-$PWD/dsdgen -scale $GEN_DATA_SCALE -dir $DATA_DIRECTORY -parallel $PARALLEL -child $CHILD -terminate n
+cd $GEN_DIR
+for i in `seq 1 10`; do
+  $GEN_DIR/dsdgen -scale $GEN_DATA_SCALE -dir $DATA_DIRECTORY -parallel $((PARALLEL * 10)) -child $(((CHILD - 1) * 10 +i)) -terminate n &
+done
+wait
 
 # make sure there is a file in each directory so that gpfdist doesn't throw an error
 declare -a tables=("call_center" "catalog_page" "catalog_returns" "catalog_sales" "customer" "customer_address" "customer_demographics" "date_dim" "household_demographics" "income_band" "inventory" "item" "promotion" "reason" "ship_mode" "store" "store_returns" "store_sales" "time_dim" "warehouse" "web_page" "web_returns" "web_sales" "web_site")
 
 for i in "${tables[@]}"; do
-	filename="$DATA_DIRECTORY/"$i"_"$CHILD"_"$PARALLEL".dat"
+  for j in `seq 1 10`; do
+	filename="$DATA_DIRECTORY/"$i"_"$(((CHILD - 1) * 10 + j))"_"$((PARALLEL * 10))".dat"
 	echo $filename
 	if [ ! -f $filename ]; then
 		touch $filename
 	fi
+  done
 done
 
 #for single nodes, you might only have a single segment but dsdgen requires at least 2
 if [ "$SINGLE_SEGMENT" -eq "1" ]; then
 	CHILD="2"
 	#build the second list of files
-	$PWD/dsdgen -scale $GEN_DATA_SCALE -dir $DATA_DIRECTORY -parallel $PARALLEL -child $CHILD -terminate n
+	$GEN_DIR/dsdgen -scale $GEN_DATA_SCALE -dir $DATA_DIRECTORY -parallel $PARALLEL -child $CHILD -terminate n
 
 	# make sure there is a file in each directory so that gpfdist doesn't throw an error
 	declare -a tables=("call_center" "catalog_page" "catalog_returns" "catalog_sales" "customer" "customer_address" "customer_demographics" "date_dim" "household_demographics" "income_band" "inventory" "item" "promotion" "reason" "ship_mode" "store" "store_returns" "store_sales" "time_dim" "warehouse" "web_page" "web_returns" "web_sales" "web_site")
